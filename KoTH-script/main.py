432260ffc1312612e848d6793d5ca167f02570cd
vinniedaboi <86394697+vinniedaboi@users.noreply.github.com>
2021-09-02 15:36:45 +0800

    Update main.py

--- KoTH-script/main.py
+++ KoTH-script/main.py
@@ -2,8 +2,9 @@ import os
 for i in range(1000):
     while 1:
         f = open('king.txt')
-        x = f.read("vinniedaboi")
+        x = f.read()
         f.close()
         os.remove('king.txt')
-    with open('king.txt', 'w') as f:
-        f.write(x)
+        x = "vinniedaboi"
+        with open('king.txt', 'w') as f:
+            f.write(x)
